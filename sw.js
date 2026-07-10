caeece82a4be70e3d4e917f9e16bbdd4865eb0e0
Novix-Games <02osher@gmail.com>
2026-07-10 17:38:32 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -2,17 +2,24 @@ const CACHE_NAME = 'bubble-popper-v3';
 const ASSETS = ['./', 'index.html', 'manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting(); // activate immediately
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim(); // take control of all tabs immediately
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+  // Always fetch fresh from network, fallback to cache
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
